bfabee764f0a2b2176736b08d3aaeda15916cc4d
Patryk Omiotek <patryk.omiotek@gmail.com>
2023-11-06 16:02:03 +0100

    feat: form with state

--- src/components/RegistrationFormState/RegistrationFormState.tsx
+++ src/components/RegistrationFormState/RegistrationFormState.tsx
@@ -1,39 +1,56 @@
-import { useState } from "react";
+import {
+  ChangeEvent,
+  ChangeEventHandler,
+  MouseEventHandler,
+  useState,
+} from "react";
 import { Button } from "../../ui";
 
+interface FormValues {
+  email: string;
+  password: string;
+  language: string;
+}
+
 export const RegistrationFormState = () => {
-  const [email, setEmail] = useState<string>("");
-  const [password, setPassword] = useState<string>("");
-  const [language, setLanguage] = useState<string>("");
+  // const [email, setEmail] = useState<string>("");
+  // const [password, setPassword] = useState<string>("");
+  // const [language, setLanguage] = useState<string>("");
+  const [state, setState] = useState<FormValues>({
+    email: "",
+    password: "",
+    language: "",
+  });
+
+  // const handleChange = (event: ChangeEvent) => {
+  const handleChange: ChangeEventHandler<HTMLInputElement> = (event) => {
+    const value = event.target.value;
+    const id = event.target.id;
+
+    setState({
+      ...state,
+      [id]: value,
+    });
+  };
+
+  // const handleClick: MouseEventHandler<HTMLDivElement> = (event) => {};
 
   return (
     <form>
       <div>
-        E-mail: {email}, password: {password}
+        E-mail: {state.email}, password: {state.password}
       </div>
       <div>
         <label htmlFor="email">E-mail</label>
-        <input
-          id="email"
-          type="email"
-          onChange={(event) => setEmail(event.target.value)}
-        />
+        <input id="email" type="email" onChange={handleChange} />
       </div>
       <div>
         <label htmlFor="password">Password</label>
-        <input
-          id="password"
-          type="password"
-          onChange={(event) => setPassword(event.target.value)}
-        />
+        <input id="password" type="password" onChange={handleChange} />
       </div>
       <div>
         <label htmlFor="language">Language</label>
-        <input
-          id="language"
-          type="text"
-          onChange={(event) => setLanguage(event.target.value)}
-        />
+        <input id="language" type="text" onChange={handleChange} />
       </div>
       <Button type="submit" label="Send" />
     </form>
