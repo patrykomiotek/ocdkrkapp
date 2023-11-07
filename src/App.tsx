2aa127a1d9b559f8abd3a4bcb15e3b5b2214e25e
Patryk Omiotek <patryk.omiotek@gmail.com>
2023-11-07 10:26:47 +0100

    feat: MagicButton component with forwardRef

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,54 @@
+import { MouseEventHandler, useEffect, useRef } from "react";
 import "./App.css";
 import { Generator } from "./components/Generator";
 import { RegistrationFormRefs } from "./components/RegistrationFormRefs/RegistrationFormRefs";
 import { RegistrationFormRefsCustomInput } from "./components/RegistrationFormRefsCustomInput";
 import { RegistrationFormState } from "./components/RegistrationFormState";
 
-import { Text, Button } from "./ui";
+import { Text, Button, MagicButton } from "./ui";
 // import { Text } from "./ui/Text/Text";
 // import { Text as Kaczka } from "./ui/Text/Text";
 
 function App() {
+  const buttonRef = useRef<HTMLButtonElement>(null);
+
+  useEffect(() => {
+    if (buttonRef.current) {
+      buttonRef.current.style.backgroundColor = "#F24727";
+    }
+  }, []);
+
+  const handleMouseEnter: MouseEventHandler<HTMLButtonElement> = () => {
+    if (buttonRef.current) {
+      buttonRef.current.style.backgroundColor = "#17A789";
+    }
+  };
+
+  const handleMouseLeave: MouseEventHandler<HTMLButtonElement> = () => {
+    if (buttonRef.current) {
+      buttonRef.current.style.backgroundColor = "#293250";
+    }
+  };
+
   return (
     <>
       {/* <h1 className="text-3xl font-bold underline">
         Hello world!
       </h1> */}
       <div>
-        <RegistrationFormRefsCustomInput />
+        <MagicButton
+          ref={buttonRef}
+          label="Click me"
+          onMouseEnter={handleMouseEnter}
+          onMouseLeave={handleMouseLeave}
+        />
+        {/* <RegistrationFormRefsCustomInput /> */}
         {/* <RegistrationFormRefs /> */}
         {/* <RegistrationFormState /> */}
-        <Generator />
-        <Text>Today is payday</Text>
-        <Button label="Click me" type="submit" />
-        <Button label="Click me" bgColor="carrot" color="emerald" />
+        {/* <Generator /> */}
+        {/* <Text>Today is payday</Text> */}
+        {/* <Button label="Click me" type="submit" /> */}
+        {/* <Button label="Click me" bgColor="carrot" color="emerald" /> */}
       </div>
     </>
   );
--- src/ui/MagicButton/MagicButton.tsx
+++ src/ui/MagicButton/MagicButton.tsx
@@ -1,12 +1,14 @@
 import {
-  useEffect,
+  forwardRef,
   type ComponentPropsWithRef,
-  useRef,
+  type Ref,
   MouseEventHandler,
 } from "react";
 
 type Props = {
   label: string;
+  onMouseEnter: MouseEventHandler<HTMLButtonElement>; // eventually get from ComponentProps and mark as required
+  onMouseLeave: MouseEventHandler<HTMLButtonElement>;
 } & ComponentPropsWithRef<"button">;
 
 // stable reference
@@ -15,43 +17,21 @@ const styles = {
   backgroundColor: "#293250",
 };
 
-export const MagicButton = ({ label, ...rest }: Props) => {
-  // new reference on each render
-  // const styles = {
-  //   color: "#ffffff",
-  //   backgroundColor: "#293250",
-  // };
-  const buttonRef = useRef<HTMLButtonElement>(null);
-  // const numberRef = useRef<number>(0);
-  // const setRef = useRef(new Set());
-
-  useEffect(() => {
-    if (buttonRef.current) {
-      buttonRef.current.style.backgroundColor = "#F24727";
-    }
-  }, []);
-
-  const handleMouseEnter: MouseEventHandler<HTMLButtonElement> = () => {
-    if (buttonRef.current) {
-      buttonRef.current.style.backgroundColor = "#17A789";
-    }
-  };
-
-  const handleMouseLeave: MouseEventHandler<HTMLButtonElement> = () => {
-    if (buttonRef.current) {
-      buttonRef.current.style.backgroundColor = "#293250";
-    }
-  };
-
-  return (
-    <button
-      ref={buttonRef}
-      style={styles}
-      onMouseEnter={handleMouseEnter}
-      onMouseLeave={handleMouseLeave}
-      {...rest}
-    >
-      {label}
-    </button>
-  );
-};
+export const MagicButton = forwardRef(
+  (
+    { label, onMouseEnter, onMouseLeave, ...rest }: Props,
+    ref: Ref<HTMLButtonElement>
+  ) => {
+    return (
+      <button
+        ref={ref}
+        style={styles}
+        onMouseEnter={onMouseEnter}
+        onMouseLeave={onMouseLeave}
+        {...rest}
+      >
+        {label}
+      </button>
+    );
+  }
+);
