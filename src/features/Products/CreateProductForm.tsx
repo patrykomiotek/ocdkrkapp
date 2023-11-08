be057f7103f5a05c91931c0ccd158697c68a95cd
Patryk Omiotek <patryk.omiotek@gmail.com>
2023-11-08 10:46:57 +0100

    feat: validation with zod

--- src/features/Products/CreateProductForm.tsx
+++ src/features/Products/CreateProductForm.tsx
@@ -1,6 +1,9 @@
 import { SubmitHandler, useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+
 import { Button, Input } from "../../ui";
-import { CreateProductDto } from "./types/CreateProductDto";
+import { CreateProductDto, validationSchema } from "./types/CreateProductDto";
+// import { CreateProductDto } from "./types/CreateProductDto";
 
 type Props = {
   onSubmit: (data: CreateProductDto) => void;
@@ -11,7 +14,9 @@ export const CreateProductForm = ({ onSubmit }: Props) => {
     register,
     handleSubmit,
     formState: { errors },
-  } = useForm<CreateProductDto>();
+  } = useForm<CreateProductDto>({
+    resolver: zodResolver(validationSchema),
+  });
 
   const createProduct: SubmitHandler<CreateProductDto> = (data) => {
     console.log(data);
@@ -22,20 +27,20 @@ export const CreateProductForm = ({ onSubmit }: Props) => {
     // eslint-disable-next-line @typescript-eslint/no-misused-promises
     <form onSubmit={handleSubmit(createProduct)}>
       <Input label="Name" {...register("name", { required: true })} />
-      {errors.name && <p className="text-red-500">Name is required</p>}
+      {errors.name && <p className="text-red-500">{errors.name.message}</p>}
       <Input
         label="Description"
         {...register("description", { required: true })}
       />
       {errors.description && (
-        <p className="text-red-500">Description is required</p>
+        <p className="text-red-500">{errors.description.message}</p>
       )}
       <Input
         label="Price"
         type="number"
         {...register("price", { required: true, valueAsNumber: true })}
       />
-      {errors.price && <p className="text-red-500">Price is required</p>}
+      {errors.price && <p className="text-red-500">{errors.price.message}</p>}
       <Button label="Submit" type="submit" />
     </form>
   );
--- src/features/Products/types/CreateProductDto.ts
+++ src/features/Products/types/CreateProductDto.ts
@@ -1,5 +1,10 @@
-export interface CreateProductDto {
-  name: string;
-  description: string;
-  price: number;
-}
+import { z } from "zod";
+
+export const validationSchema = z.object({
+  name: z.string().min(1, { message: "Name is required" }),
+  description: z.string().min(1, { message: "Description is required" }),
+  price: z.number().positive({ message: "Price must be be positive 😄" }),
+});
+
+// type Validation = typeof validationSchema;
+export type CreateProductDto = z.infer<typeof validationSchema>;
