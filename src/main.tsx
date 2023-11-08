d32bb0cc3f47a63e57581abb0b13910d2b4585c5
Patryk Omiotek <patryk.omiotek@gmail.com>
2023-11-08 14:34:08 +0100

    feat: msw downgrade do 1.x

--- src/main.tsx
+++ src/main.tsx
@@ -2,29 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+import { worker } from "./mocks/browser";
 
-async function deferRender() {
-  if (process.env.NODE_ENV !== "development") {
-    return;
-  }
-
-  const { worker } = await import("./mocks/browser");
-
-  // `worker.start()` returns a Promise that resolves
-  // once the Service Worker is up and ready to intercept requests.
-  return worker.start();
+// Start the mocking conditionally.
+if (process.env.NODE_ENV === "development") {
+  // const { worker } = require("./mocks/browser");
+  // eslint-disable-next-line @typescript-eslint/no-floating-promises
+  worker.start();
 }
 
-void deferRender().then(() => {
-  ReactDOM.createRoot(document.getElementById("root")!).render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  );
-});
-
-// ReactDOM.createRoot(document.getElementById('root')!).render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>,
-// )
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
